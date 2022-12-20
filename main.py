26a8f263d7ab2d3a8f720adf8ebcaf0cf9a736b8
Алиев Ибрагим Мубариз <Aliev_ox53@itstep.edu.az>
2022-12-20 17:44:22 +0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# user_number = 0
+#
+# while True:
+#     user_number = int(input("Enter a digit:"))
+#     print(user_number + int(input("Enter a digit:")))
+#     if user_number == 0:
+#         print("You have written 0,and your program was stopped.")
+#         break
+
+# Task 2
+# user_number = 0
+#
+# while True:
+#     user_number = int(input("Enter your number:"))
+#     if user_number % 3 == 0:
+#         print(user_number)
+#     if 1 < user_number <= 30:
+#         print(user_number)
+#     if user_number == 0:
+#         print("Your number is 0.Program was stop.")
